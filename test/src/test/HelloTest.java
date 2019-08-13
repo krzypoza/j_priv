9b79cc7e2a965d9518f47e01f43d53d615ed4f71
KrzyPoza <jasperr99@gmail.com>
2019-08-13 15:03:53 +0200

    4 first exercise

--- test/src/test/HelloTest.java
+++ test/src/test/HelloTest.java
@@ -3,8 +3,14 @@ package test;
 public class HelloTest {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		
+		System.out.println(SpeedConverter.toMilesPerHour(10.5)); // SpeedConverter
+		SpeedConverter.printConversion(-2); // SpeedConverter
 
+		MegaBytesConverter.printMegaBytesAndKiloBytes(5000); // MegaBytesConverter
+
+		System.out.println(BarkingDog.shouldWakeUp(true, 0));// BarkingDog
+		System.out.println("isLeapYear " + LeapYear.isLeapYear(1700));
 	}
 
 }
---
+++ test/src/test/LeapYear.java
@@ -0,0 +1,14 @@
+package test;
+
+public class LeapYear {
+
+	public static boolean isLeapYear(int year) {
+		if (year < 1 || year > 9999) {
+			return false;
+		} else if (year % 4 == 0 && year % 400 == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
