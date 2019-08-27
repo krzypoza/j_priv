c166a9fb8d07c862c9f6595490c1c1cd5b0ec340
KrzyPoza <jasperr99@gmail.com>
2019-08-27 09:48:45 +0200

    before 70

--- test/src/test/HelloTest.java
+++ test/src/test/HelloTest.java
@@ -10,7 +10,19 @@ public class HelloTest {
 		MegaBytesConverter.printMegaBytesAndKiloBytes(5000); // MegaBytesConverter
 
 		System.out.println(BarkingDog.shouldWakeUp(true, 0));// BarkingDog
-		System.out.println("isLeapYear " + LeapYear.isLeapYear(1700));
+		System.out.println("isLeapYear " + LeapYear.isLeapYear(1700));// LeapYear
+		System.out.println("DecimalComparator " + DecimalComparator.areEqualByThreeDecimalPlaces(1.2568, 1.256));
+		System.out.println("EqualSumChecker " + EqualSumChecker.hasEqualSum(1, -1, 0));// EqualSumChecker
+		System.out.println("TeenNumberChecker " + TeenNumberChecker.hasTeen(12, 14, 45));
+		System.out.println("TeenNumberChecker " + TeenNumberChecker.isTeen(13));
+		System.out.println(AreaCalculator.area(1));
+		System.out.println(AreaCalculator.area(2, 5));
+		MinutesToYearsDaysCalculator.printYearsAndDays(1441);
+		IntEqualityPrinter.printEqual(3, 3, 2);
+		System.out.println(PlayingCat.isCatPlaying(true, 10));
+		NumberInWord.printNumberInWord(4);
+		System.out.println(SumOddRange.sumOdd(100, 1000));
+		System.out.println(NumberPalindrome.isPalindrome(12121));
 	}
 
 }
